788b562d6b81058783c5c7691153eec5fb58aeda
Divyanshu Mishra <keshumishra987@gmail.com>
2024-05-04 22:59:26 +0530

    added movement

--- main.js
+++ main.js
@@ -4,15 +4,37 @@ class Game {
     this.ctx = context;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+    this.baseHeight=720;
+    
+    this.ratio=this.height/this.baseHeight;
     this.player = new Player(this);
+    this.resize(window.innerWidth,window.innerHeight);
+    window.addEventListener("resize",e=>{
+      this.resize(e.currentTarget.innerWidth,canvas.height=e.currentTarget.innerHeight);
+    });
+    this.canvas.addEventListener("mousedown",e=>{
+      this.player.flap(); 
+    })
+  }
 
+  resize(width,height){
+    this.canvas.width=width;
+    this.canvas.height=height;
+    this.ctx.fillStyle="gold";
+    this.width=this.canvas.width;
+    this.height=this.canvas.height;
+    this.ratio=this.height/this.baseHeight;
+    this.gravity=0.15*this.ratio;
+    this.player.resize();
+    
   }
   render() {
-    this.ctx.fillStyle = "red";
+    
     this.player.update();
     this.player.draw();
   }
 }
+///////////////////////////////////////////////////////
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
@@ -26,6 +48,7 @@ window.addEventListener("load", function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.render();
     requestAnimationFrame(animate);
-  } this.requestAnimationFrame(animate);
+  } 
+  this.requestAnimationFrame(animate);
 
 });
