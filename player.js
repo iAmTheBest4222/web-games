4bb6e45be7b528b22433d297038be105b7c61a27
Divyanshu Mishra <keshumishra987@gmail.com>
2024-05-04 23:12:41 +0530

    error solve

--- player.js
+++ player.js
@@ -5,34 +5,34 @@ class Player {
         this.y;
         this.height = 200;
         this.width = 200;
-        this.spriteHeight=200;
-        this.spritewidth=200;
-        this.speedY=-2;
+        this.spriteHeight = 200;
+        this.spritewidth = 200;
+        this.speedY = -2;
     } draw() {
         this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
     } update() {
         this.x++;
-        this.y+=this.speedY;
+        this.y += this.speedY;
         if (!this.isTouchingBottom()) {
-            this.speedY+=this.game.gravity;
-        }if (this.isTouchingBottom()) {
-            this.y=this.game.height-this.height;
+            this.speedY += this.game.gravity;
+        } if (this.isTouchingBottom()) {
+            this.y = this.game.height - this.height;
         }
-    }resize(){
-        this.width=this.spriteHeight*this.game.ratio;
-        this.height=this.spriteHeight*this.game.ratio;
-        this.y=this.game.height*0.5-this.height*0.5;
-        this.speedY=-8*this.game.ratio;
+    } resize() {
+        this.width = this.spriteHeight * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+        this.y = this.game.height * 0.5 - this.height * 0.5;
+        this.speedY = -8 * this.game.ratio;
     }
-    isTouchingBottom(){
-        return this.y>=this.game.height-this.height
+    isTouchingBottom() {
+        return this.y >= this.game.height - this.height
     }
-    isTouchingTop(){
-        return this.y<=0;
+    isTouchingTop() {
+        return this.y <= 0;
     }
-    flap(){
+    flap() {
         if (!this.isTouchingTop()) {
-            this.speedY=-5;
+            this.speedY = -5;
         }
     }
 }
